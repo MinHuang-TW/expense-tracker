7f8a872ce6e6426360bd509fa873ea63743701c1
Min Huang <h.min719@gmail.com>
2020-03-20 18:12:02 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction.jsx';
+import { GlobalProvider } from './context/GlobalState.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
